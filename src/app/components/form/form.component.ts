7e1ed202c85b5f306fd7ea4a30d1d16511d41ffd
joubits <joubits.dw@gmail.com>
2019-08-29 06:50:06 -0500

    Refactor getList of Task2

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -18,26 +18,28 @@ export class FormComponent implements OnInit {
   id: any;
   editing:boolean = false;
 
+  error;
+  success;
   constructor( private taskService: TasksService, private activatedRoute: ActivatedRoute  ) {
     this.id = this.activatedRoute.snapshot.params['id'];
-    //if(this.id){
-      //this.editing = true;
+    if(this.id){
+      this.editing = true;
       this.taskService.readTask(this.id);
       this.task = taskService.task;
 
-    //}else {
-      //this.editing = false;
-    //}
+    }else {
+      this.editing = false;
+    }
   }
 
   ngOnInit() {
   }
 
-  saveTask($event, form) {
+  saveTask($event) {
     console.log("Se va a guardar tarea?");
     $event.preventDefault();
-    if(form.task.id) {
-      this.taskService.put(form.task).subscribe( (data)=> {
+    if(this.editing) {
+      this.taskService.put(this.task).subscribe( (data)=> {
         alert("Task is updated");
         console.log(data);
       }, (error) => { 
@@ -45,15 +47,36 @@ export class FormComponent implements OnInit {
         alert("An error happend");
       });
     } else {
-      this.taskService.save(form.task).subscribe( (data)=> {
-        alert("Task is created");
-        console.log(data);
-      }, (error) => { 
-        console.log(error);
-        alert("An error happend");
-      });
+      if(this.task.body){
+        this.taskService.save(this.task).subscribe( (data)=> {
+          alert("Task is created");
+          console.log(data);
+        }, (error) => { 
+          console.log(error);
+          alert("An error happend");
+        });
+      }
     }
-    
   }
+
+  // addTask(f) {
+  //   this.error = '';
+  //   this.success = '';
+
+  //   this.taskService.store(this.task)
+  //     .subscribe(
+  //       (res: Task) => {
+  //         // Update the list of cars
+  //         this.task = res;
+
+  //         // Inform the user
+  //         this.success = 'Created successfully';
+
+  //         // Reset the form
+  //         f.reset();
+  //       },
+  //       (err) => this.error = err
+  //     );
+  // }
 
 }
